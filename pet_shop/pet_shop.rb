fc97de07851617a5d1f106fbc120acfb4c5b00f0
kimnorth <benignhumour@gmail.com>
2017-02-03 10:53:37 +0000

    5) Pets Sold - done

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -10,3 +10,7 @@ def add_or_remove_cash(shop, cash_amount)
   new_total = shop[:admin][:total_cash] += cash_amount
   return new_total
 end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
+end
